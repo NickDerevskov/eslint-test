6e4ace4fd3ab9dcb885c45f5cb6bd94816a7b336
Nikita Derevskov <nikitaderevskov@gmail.com>
2022-08-01 07:58:20 +0300

    src: add test files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import test1 from './test1.ts';
+import test2 from './test2';
+import test3 from './test3';
+import test4 from './test4';
+
+console.log(test1, test2, test3, test4);
---
+++ src/test2.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function test() {
+  console.log('test');
+
+  return <div>lets go</div>;
+}
+
+test();
